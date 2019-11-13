cd61a2a5e7c5b7fff9e80dbf2925b985312edc23
Pagani Walter <paganiwalter@gmail.com>
2019-11-13 04:39:32 -0300

    Cambios generales de diseño y agregando urls de las aplicaciones

--- epet20/urls.py
+++ epet20/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
     path('asesoria/', include('apps.asesoria.urls')),
+    path('secretaria/', include('apps.secretaria.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
