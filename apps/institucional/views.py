49916743cf9015fed353f8ef1cecd6cb52734b85
Pagani Walter <paganiwalter@hotmail.com>
2019-11-13 15:24:42 -0300

    Mostrando institucional junto con las demas aplicaciones

--- apps/institucional/views.py
+++ apps/institucional/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-
 from .models import Seguimiento
+
 
 class IndexView(ListView):
 	model = Seguimiento
